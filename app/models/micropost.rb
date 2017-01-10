cb835424abba3db6652985922ae1dbaf7c02a007
sanariaz154 <sanariaz154@gmail.com>
2017-01-11 04:25:28 +0500

    modified micropost.rb by removing error correctly.  added validation to user to only use 140 charecters

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,16 @@
 class Micropost < ActiveRecord::Base
-attr_accessible :content, :user_id
+
+
+def create
+  Category.create(category_params)
+end
+
+private
+
+def category_params
+  params.require(:category).permit(:content)
+  params.require(:category).permit(:user_id)
+end
+
 validates :content, :length => { :maximum => 140 }
 end
